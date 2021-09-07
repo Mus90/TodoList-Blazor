f6c476db8cce2386313d56bd55d7502848a7a6de
mustafa <mustafaalamin.07@gmail.com>
2021-09-07 11:26:18 +0200

    * MyDbContext.cs: * Counter.razor: * WeatherForecast.cs: * SignInRequest.cs: * SignUpRequest.cs: * RegistrationRequest.cs: * WeatherForecastService.cs: * 20210907091116_initial.cs: * 20210907091116_initial.Designer.cs:
    * appsettings.json: Added Blazor fronend

--- todo-backend/Controllers/SignInRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace todo_backend.Controllers
-{
-    public class SignInRequest
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
---
+++ todo-backend/Migrations/20210907091116_initial.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace todo_backend.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_TodoLists_TodoListID",
+                table: "Items");
+
+            migrationBuilder.RenameColumn(
+                name: "TodoListID",
+                table: "Items",
+                newName: "TodoListId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Items_TodoListID",
+                table: "Items",
+                newName: "IX_Items_TodoListId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TodoListId",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_TodoLists_TodoListId",
+                table: "Items",
+                column: "TodoListId",
+                principalTable: "TodoLists",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_TodoLists_TodoListId",
+                table: "Items");
+
+            migrationBuilder.RenameColumn(
+                name: "TodoListId",
+                table: "Items",
+                newName: "TodoListID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Items_TodoListId",
+                table: "Items",
+                newName: "IX_Items_TodoListID");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TodoListID",
+                table: "Items",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_TodoLists_TodoListID",
+                table: "Items",
+                column: "TodoListID",
+                principalTable: "TodoLists",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
